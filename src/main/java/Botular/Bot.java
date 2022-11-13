ac8587d7c435e083934a1b0fb44bd6367aa37421
tothert <74312911+tothert@users.noreply.github.com>
2022-11-13 18:39:58 -0500

    Add files via upload

---
+++ src/main/java/Botular/Bot.java
@@ -0,0 +1,11 @@
+package Botular;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Bot {
+    public static void main(String[] arguments) throws Exception
+    {
+        JDA api = JDABuilder.createDefault("why u tryna find my token bro").build();
+
+    }
